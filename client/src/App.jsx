34943cb4a7bad577871e9a28cffbb5a7c2aab9ac
Arpit Sachdeva <arpitbodybuilder@gmail.com>
2026-08-15 19:06:21 +0530

    Change default route to LaptopPage and add Navigate
    Updated routing to set LaptopPage as the default and added navigation for unknown routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LaptopPage from "./pages/LaptopPage";
 import MobilePage from "./pages/MobilePage";
 
@@ -6,9 +6,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MobilePage />} />
+        {/* Main desktop/laptop application */}
+        <Route path="/" element={<LaptopPage />} />
+
+        {/* Mobile remote controller */}
         <Route path="/mobile" element={<MobilePage />} />
+
+        {/* Optional direct laptop route */}
         <Route path="/laptop" element={<LaptopPage />} />
+
+        {/* Unknown routes go back to laptop page */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
